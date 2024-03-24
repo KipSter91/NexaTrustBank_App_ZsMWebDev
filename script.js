585e6f73a948b00982fadaf20a8a7be299ea8ecd
Zsolt Marku <zsolt.marku910221@gmail.com>
2024-03-24 18:05:04 +0100

    Attributes (lecture)

--- script.js
+++ script.js
@@ -36,3 +36,50 @@ header.append(message); // Add the element as the last child of the header eleme
 
 // Delete Elements
 document.querySelector('.btn--close-cookie').addEventListener('click', () => message.remove());
+
+// Styles, Attributes, and Classes
+
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '100vw';
+
+console.log(message.style.color); // Returns the inline style of the element
+console.log(message.style.backgroundColor); // Returns the inline style of the element
+
+console.log(getComputedStyle(message).color); // Returns the computed style of the element
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+//Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt); // Returns the value of the alt attribute
+
+console.log(logo.src); // Returns the absolute URL of the src attribute
+console.log(logo.getAttribute('src')); // Returns the relative URL of the src attribute
+
+logo.setAttribute('alt', 'Fantastic NexaTrust logo'); // Sets the value of the alt attribute
+console.log(logo.getAttribute('alt')); // Returns the value of the alt attribute
+
+//Non-standard attributes
+console.log(logo.getAttribute('designer'));
+
+logo.setAttribute('company', 'ZsMWebDev');
+console.log(logo.getAttribute('company'));
+
+const link = document.getElementById('portfolio');
+console.log(link.href); // Returns the absolute URL of the href attribute
+
+//Data attributes
+console.log(logo.dataset.versionNumber); // Returns the value of the data-version-number attribute
+
+//Classes
+logo.classList.add('c', 'j'); // Adds the classes 'c' and 'j' to the element you can add multiple classes at once by separating them with a comma
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c', 'j');
+logo.classList.contains('c', 'j');
+
+// Don't use this method because it will overwrite all existing classes
+logo.className = 'zsolt';
+logo.className = 'nav__logo';
